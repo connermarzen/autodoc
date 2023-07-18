9d65758275c31bae6c1619f665425021edb19a6d
Conner Marzen <connermarzen@gmail.com>
2023-07-17 23:42:35 -0500

    Removed positional dir argument (now optional)

--- docwalker/__main__.py
+++ docwalker/__main__.py
@@ -4,8 +4,8 @@ from docwalker.cli import PARSER, generate_config
 
 def main():
     args = PARSER.parse_args()
-    config = generate_config(f"{args.src_dir}/{args.file}" if args.file else ".dwconf")
-    app = ViewerApp(args.src_dir, config)
+    config = generate_config(f"{args.dir}/{args.file}" if args.file else ".dwconf")
+    app = ViewerApp(args.dir, config)
     app.run()
 
 
